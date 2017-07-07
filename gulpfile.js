2eec6b20895ed07622baae03d71ecdec1e3d6fa3
willewerton <william_ewerton@outlook.com>
2017-07-07 10:11:54 -0300

    config

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
-gulp.task('styles', function() {
+gulp.task('style', function() {
     gulp.src('sass/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 });
 
+gulp.task('config', function(){
+  gulp.src("./config.exemple.php")
+  .pipe(rename("./config.php"))
+  .pipe(gulp.dest("./"));
+});
+
 
 gulp.task('default', function() {
-  gulp.watch('sass/style.scss',['styles']);
+  gulp.watch('sass/style.scss',['style']);
 
-//   gulp.src('./config.exemple.php')
-//         .pipe(gulp.dest('./', ['config.php']));
 });
